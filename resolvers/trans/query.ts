37350b4b439ed9a1a288bfd8ed258e957196b88d
leenorshn <katembovictor@gmail.com>
2022-09-08 12:13:42 +0200

    ok

--- resolvers/trans/query.ts
+++ resolvers/trans/query.ts
@@ -2,23 +2,6 @@ import prisma from "../../lib/prisma";
 import getUser from "../../utils/getUser";
 
 const transQuery = {
-  getBalance: async (_, { uid }) => {
-    var trans = await prisma.trans.findMany({
-      where: { OR: [{ senderRef: uid }, { receiverRef: uid }] },
-    });
-    let balanceSended = 0;
-    let balanceReceived = 0;
-    for (var i = 0; i < trans.length; i++) {
-      if (trans[i].status == "SEND") {
-        balanceSended = balanceSended + trans[i].amount.amount;
-      }
-      if (trans[i].status == "RECEIVE") {
-        balanceReceived = balanceReceived + trans[i].amount.amount;
-      }
-    }
-
-    return { amount: balanceReceived - balanceSended, currency: "USD" };
-  },
   allTrans: async (_, { uid }) => {
     if (uid) {
       return prisma.trans.findMany({
